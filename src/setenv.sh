498b4eb2fc20977d7e803bea861f27d64725c57e
joveli <jove@163.com>
2019-12-07 13:48:38 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=joveli
 
 export MYCLUSTER=tektonknativebeijing06
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing06/kube-config-tok02-tektonknativebeijing06.yml
 
 export NAMESPACE=ddcnlab
 export EMAIL=jove@163.com
